407f2b4d960f1c4ae420e9698528a512d418cda1
Bruno Mendes <brpimendes@gmail.com>
2019-07-22 22:02:09 -0300

    Removida operações que eram desnecessárias.

--- AreaTriangulo/Program.cs
+++ AreaTriangulo/Program.cs
@@ -3,35 +3,7 @@ using System.Globalization;
 
 namespace AreaTriangulo {
     class Program {
-        static void Main(string[] args) {
-            /*double xA, xB, xC, yA, yB, yC;
-
-            Console.WriteLine("Entre com as medidas do triângulo X:");
-            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Console.WriteLine("Entre com as medidas do triângulo Y:");
-            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            double p = (xA + xB + xC) /  2.00;
-            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
-
-            p = (yA + yB + yC) / 2.00;
-            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
-
-            Console.WriteLine("A área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
-            Console.WriteLine("A área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
-
-            if (areaX > areaY) {
-                Console.WriteLine("A maior área é: X");
-            }
-            else {
-                Console.WriteLine("A maior área é: Y");
-            }*/
-
+        static void Main(string[] args) {            
             Triangulo x, y;
 
             x = new Triangulo();
